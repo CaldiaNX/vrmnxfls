3fd37d935b955a3a10328aa7a3019158c31c3506
Caldia <regretblade@gmail.com>
2020-11-14 14:16:42 +0900

    v1.2 2020/09/23 レイアウト情報出力「sendSettingFile」追加

--- vrmnxfls.py
+++ vrmnxfls.py
@@ -5,6 +5,8 @@
 
 import vrmapi
 import shutil
+import os.path
+from datetime import datetime
 from pathlib import Path
 
 #ファイル読み込みの確認用
@@ -37,7 +39,7 @@ def readFile():
             #vrmapi.LOG(str(re))
             shutil.move(item, re)
 
-# ファイル連携システム命令セット
+# FLS命令セット
 def readFileLine(line):
     #vrmapi.LOG(line[0])
     # 種別T＝列車オブジェクト
@@ -55,3 +57,38 @@ def readFileLine(line):
         point = vrmapi.LAYOUT().GetPoint(int(line[1]))
         if line[2] == "SetBranch":
             point.SetBranch(int(line[3]))
+
+# FLSレイアウト情報出力
+def sendSettingFile():
+    if(os.path.exists('send\\') == False):
+        #vrmapi.LOG("vrmnxfls.sendSettingFile is no find send\\ folder.")
+        return
+
+    s = list()
+
+    #編成リストを新規編成リストに格納
+    tList = vrmapi.LAYOUT().GetTrainList()
+    #新規編成リストから編成を繰り返し取得
+    for t in tList:
+        pos = t.GetPosition()
+        s.append('t\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(t.GetID(), t.GetNAME(), pos[0], pos[1], pos[2], t.GetVoltage()))
+    s.append('\n')
+
+    #新規ポイントリストを作成
+    pList=list()
+    #ポイントリストを新規ポイントリストに格納
+    vrmapi.LAYOUT().ListPoint(pList)
+    #新規ポイントリストからポイントを繰り返し取得
+    for p in pList:
+        pos = p.GetPosition()
+        s.append('p\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}\n'.format(p.GetID(), p.GetNAME(), pos[0], pos[1], pos[2], p.GetBranch()))
+    s.append('\n')
+
+    # 結合
+    text = ''.join(s)
+    # ファイル出力
+    timeText = datetime.now().strftime('%Y%m%d_%H%M%S%f')
+    path_w = 'send\\' + timeText + '.txt'
+    with open(path_w, mode='w') as f:
+        f.write(text)
+        vrmapi.LOG("sendフォルダにレイアウト情報ファイルを出力しました。")
